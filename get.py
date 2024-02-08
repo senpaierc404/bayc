d772f36e892072ffc5e3ecf12c59c59af49d010c
Nghia Hoang <hoangnghia1092002@gmail.com>
2024-02-08 13:34:03 -0500

    Upload file

--- get.py
+++ get.py
@@ -3,5 +3,5 @@ import requests
 for i in range(100):
     image_url = 'https://boredapeyachtclub.com/_next/image?url=https%3A%2F%2Fassets.boredapeyachtclub.com%2Fbayc%2F' + str(i) + '.png&w=640&q=75'
     img_data = requests.get(image_url).content
-    with open('bayc' +  str(i) + '.jpg', 'wb') as handler:
+    with open('bayc' +  str(i) + '.png', 'wb') as handler:
         handler.write(img_data)
